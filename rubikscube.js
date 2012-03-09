b1b7ace7b489aa01ed83f324c04a96e387b2f8a2
Pierre-Antoine LaFayette <pierre.lafayette@gmail.com>
2012-03-09 01:39:41 -0500

    Working sub-cube.

--- rubikscube.js
+++ rubikscube.js
@@ -4,7 +4,7 @@ var Cube = function(elementID) {
     this.COLUMNS = 3;
     //this.subCubes = new Array(this.DEPTH * this.ROWS * this.COLUMNS);
     this.subCubes = new Array(1);
-    this.element = $("#"+elementID); 
+    this.element = $("#"+elementID);
 
     this.reset = function() {
     };
@@ -19,8 +19,8 @@ var Cube = function(elementID) {
     };
 
     for (var i = 0; i < this.subCubes.length; ++i) {
-    this.subCubes[i] = new SubCube(i, this.element);
-    this.element.append(this.subCubes[i]);
+        this.subCubes[i] = new SubCube(i, this.element);
+        this.element.append(this.subCubes[i]);
     }
 
     return true;
@@ -28,35 +28,33 @@ var Cube = function(elementID) {
 
 var SubCube = function(index, parentElement) {
     this.element = $('<div/>', {
-    id: 'subCube' + index,
-    class: 'subCube cubeContainer'
+        id: 'subCube' + index,
+        class: 'subCube'
     });
 
-
-    var ul = $('<ul class="cube left">');
-    ul.append($('<li class="front" data-label="Front">Front</li>'));
-    ul.append($('<li class="left" data-label="Left">Left</li>'));
-    ul.append($('<li class="back" data-label="Back">Back</li>'));
-    ul.append($('<li class="right" data-label="Right">Right</li>'));
-    ul.append($('<li class="bottom" data-label="Bottom">Bottom</li>'));
-    ul.append($('<li class="top" data-label="Top">Top</li>'));
-    this.element.append(ul);
+    // Add the 6 sides of the cube.
+    this.element.append($('<div class="front" />'));
+    this.element.append($('<div class="left" />'));
+    this.element.append($('<div class="back" />'));
+    this.element.append($('<div class="right" />'));
+    this.element.append($('<div class="bottom" />'));
+    this.element.append($('<div class="top" />'));
 
     this.element.appendTo(parentElement);
+
     return true;
 };
 
 var init = function() {
     var hasPerspective = false;
     testProperties = ['perspectiveProperty',
-                   'WebkitPerspective',
-                   'MozPerspective',
-                   'OPerspective',
-                   'msPerspective'];
+                      'WebkitPerspective',
+                      'MozPerspective',
+                      'OPerspective',
+                      'msPerspective'];
 
     for (var i = 0; i < testProperties.length; ++i) {
         if (document.documentElement.style[testProperties[i]] !== undefined) {
-            document.documentElement.className += ' perspective';
             hasPerspective = true;
             break;
         }
@@ -66,11 +64,17 @@ var init = function() {
         var rubiksCube = new Cube('rubiksCube');
 
         function rotate(direction) {
-            $($('.cube')[0]).removeClass().addClass('cube ' + direction);
+            $('.subCube').each( function() { $(this).removeClass().addClass('subCube ' + direction + 'Transform'); });
         }
 
         $(document).keydown(function(e){
             switch(e.keyCode) {
+                case 66:
+                  rotate('back');
+                  break;
+                case 70:
+                  rotate('front');
+                  break;
                 case 37:
                   rotate('left');
                   break;
@@ -85,9 +89,7 @@ var init = function() {
                   break;
             };
         });
-
-    }
-    else {
+    } else {
         $('body').append("Your browser does not support CSS 3D transforms. Sorry :(");
     }
 }();
